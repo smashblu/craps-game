0bc395ca0e2d04e1f5ed55e0d5eeeda1b03053d3
smashblu <smashblu@gmail.com>
2024-02-18 14:56:58 -0800

    Add JS functions, map clickable board

--- resources/scripts.js
+++ resources/scripts.js
@@ -1,1 +1,15 @@
-// Game scripts
+function diceroll() {
+    let dieOne = getRandomInt(1,7); 
+    let dieTwo = getRandomInt(1,7); 
+    playerRoll = dieOne + dieTwo;
+    document.getElementById("show-roll").innerHTML = `${dieOne} + ${dieTwo} = ${playerRoll}`;
+}
+function getRandomInt(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+}
+function boardClick(num) {
+    document.getElementById("clickNum").innerHTML = num;
+}
+let playerRoll = 0;
