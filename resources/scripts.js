45649298d41d3ba328da57136194a5b9b6bcf11e
smashblu <smashblu@gmail.com>
2024-02-22 08:56:10 -0800

    Use event listeners

--- resources/scripts.js
+++ resources/scripts.js
@@ -1,4 +1,5 @@
-function diceroll() {
+function diceRoll(e) {
+    console.log("rolled", e.target.textContent);
     let dieOne = getRandomInt(1,7); 
     let dieTwo = getRandomInt(1,7); 
     playerRoll = dieOne + dieTwo;
@@ -13,3 +14,4 @@ function boardClick(num) {
     document.getElementById("clickNum").innerHTML = num;
 }
 let playerRoll = 0;
+document.addEventListener("copy", diceRoll);
