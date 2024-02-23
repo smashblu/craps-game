10a25d654c9f68f22fab60fe585870afb21d9abe
smashblu <smashblu@gmail.com>
2024-02-23 14:39:06 -0800

    Make suggested changes for readability

--- resources/scripts.js
+++ resources/scripts.js
@@ -1,20 +1,23 @@
 function diceRoll() {
-    let dieOne = getRandomInt(1,7); 
-    let dieTwo = getRandomInt(1,7); 
+    const dieOne = getRandomInt(1,7); 
+    const dieTwo = getRandomInt(1,7); 
     playerRoll = dieOne + dieTwo;
     document.getElementById('show-roll').innerHTML = `${dieOne} + ${dieTwo} = ${playerRoll}`;
 }
+
 function getRandomInt(min, max) {
   const minCeiled = Math.ceil(min);
   const maxFloored = Math.floor(max);
   return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
 }
+
 function boardClick(num) {
     document.getElementById('clickNum').innerHTML = num;
 }
+
 let playerMoney = 100;
 let playerRoll = 0;
-const rollDiceButton = document.getElementById('rollbutton')
+const rollDiceButton = document.getElementById('roll-button')
 const maxBetInput = document.querySelector('.betamount');
 rollDiceButton.addEventListener('click', diceRoll);
 maxBetInput.setAttribute('max', playerMoney);
