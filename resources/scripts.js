b9601040035140dbaf072223a7e3f0b05fe34052
smashblu <smashblu@gmail.com>
2024-08-31 13:33:51 -0700

    Simplify button states

--- resources/scripts.js
+++ resources/scripts.js
@@ -9,12 +9,15 @@ function newGame() {
     lastPlayerMoney = playerMoney;
     playerMoney = 100;
     playerMoneyChange();
-    betAmount = 0;
-    buttonPosition(1);
+    resetGame();
     displayMessage('New game started');
-    rollButtonState(false);
-    betButtonState(true);
-    comeButtonState(false);
+}
+
+function resetGame() {
+    playerRoll = 1;
+    buttonPosition(playerRoll);
+    betAmount = 0;
+    buttonStates();
 }
 
 function saveGame() {
@@ -29,32 +32,25 @@ function loadGame() {
     onButton.style.left = '85px';
 }
 
-function rollButtonState(enabled) {
-    if (enabled === true) {
-        rollButtonElement.disabled = false;
-        return;
-    }
-    rollButtonElement.disabled = true;
-}
-
-function betButtonState(enabled) {
-    betDialogElement.setAttribute('max', playerMoney);
-    playerMoneyChange();
-    if (enabled === true) {
+function buttonStates() {
+    if (betAmount === 0) {
+        console.log('if true');
+        rollButtonElement.disabled = true;
         betDialogElement.disabled = false;
         betButtonElement.disabled = false;
         return;
     }
+    console.log('bypass if');
+    betDialogElement.setAttribute('max', playerMoney);
+    playerMoneyChange();
+    rollButtonElement.disabled = false;
     betDialogElement.disabled = true;
     betButtonElement.disabled = true;
-}
-
-function comeButtonState(enabled) {
-    if (enabled === true) {
-        comeButtonElement.disabled = false;
-        return;
-    }
     comeButtonElement.disabled = true;
+    if (pointOpen === true) {
+        comeButtonElement.disabled = false;
+    }
+    return;
 }
 
 async function firstRoll() {
@@ -73,6 +69,7 @@ async function firstRoll() {
     pointOpen = true;
     buttonPosition(playerRoll);
     displayMessage(`Your point is ${playerPoint}`);
+    buttonStates();
 }
 
 async function gameRoll() {
@@ -155,10 +152,8 @@ function playerWin() {
     lastPlayerMoney = playerMoney;
     playerMoney += (betAmount * 2);
     playerMoneyChange();
-    buttonPosition(1);
     betAmount = 0;
-    rollButtonState(false);
-    betButtonState(true);
+    buttonStates();
 }
 
 function playerLose() {
@@ -167,13 +162,14 @@ function playerLose() {
         gameOver();
         return;
     }
-    buttonPosition(1);
+    playerRoll = 1;
+    buttonPosition(playerRoll);
     betAmount = 0;
-    rollButtonState(false);
-    betButtonState(true);
+    buttonStates();
 }
 
-function makeBet() {
+function makeBet(e) {
+    console.log(e);
     betAmount = betDialogElement.value;
     betAmount = parseInt(betAmount);
     if (validateBet(betAmount) === false) {
@@ -182,16 +178,20 @@ function makeBet() {
     lastPlayerMoney = playerMoney;
     playerMoney -= betAmount;
     playerMoneyChange();
-    rollButtonState(true);
-    betButtonState(false);
+    console.log(betAmount);
+    buttonStates();
     return;
+}
+
+function makeCome(e) {
+    console.log('Not yet implemented');
 }
 
 function gameOver() {
     displayMessage(`You are bankrupt! Please choose 'New Game' from the menu to play again`);
-    buttonPosition(1);
-    rollButtonState(false);
-    betButtonState(false);
+    playerPoint = 1;
+    buttonPosition(playerRoll);
+    buttonStates();
 }
 
 async function playerMoneyChange() {
@@ -260,10 +260,12 @@ function getRandomInt(min, max) {
 
 let playerMoney = 100;
 let lastPlayerMoney = 0;
-let playerRoll = 0;
+let playerRoll = 1;
 let pointOpen = false;
 let playerPoint = 0;
 let betAmount = 0;
+let comeAmount = 0;
+let placeAmount = 0;
 let firstViewed = true;
 
 const playerMoneyElement = document.getElementById('player-money');
@@ -288,20 +290,21 @@ const secondDieElement = document.getElementById('second-die');
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 let tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
-rollButtonElement.addEventListener('click', diceRoll);
-clickBoardNumber.addEventListener('click', boardClick);
-buttonPosition(1);
-rollButtonState(false);
-betButtonState(false);
-comeButtonState(false);
+buttonPosition(playerRoll);
+rollButtonElement.disabled = true;
+betButtonElement.disabled = true;
+comeButtonElement.disabled = true;
+
 document.querySelector('#load-game').addEventListener('click', loadGame);
 document.querySelector('#save-game').addEventListener('click', saveGame);
+rollButtonElement.addEventListener('click', diceRoll);
 rollButtonElement.addEventListener('click', checkGameState);
-document.querySelector('#bet-button').addEventListener('click', makeBet);
+betButtonElement.addEventListener('click', makeBet);
+clickBoardNumber.addEventListener('click', boardClick);
+comeButtonElement.addEventListener('click', makeCome);
 
 for (let i = 0; i < newGameButtons.length; i++) {
     newGameButtons[i].addEventListener('click', newGame);
 }
 
-chipChange(2, 1);
 firstGame();
