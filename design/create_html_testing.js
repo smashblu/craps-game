632de27be6ab19b29bec99a93e15ca84ea016111
smashblu <smashblu@gmail.com>
2024-09-09 16:20:28 -0700

    Additional object creation testing

--- design/create_html_testing.js
+++ design/create_html_testing.js
@@ -1,5 +1,16 @@
-function createObj {
-        // Create object
+function iterateObj(id) {
+        numOfObj++
+        const newDiv = document.createElement('div');
+        const newContent = document.createTextNode(`Object #${id}`);
+        newDiv.appendChild(newContent);
+        document.body.insertBefore(newDiv, firstObj);;
+        newDiv.setAttribute('id', `obj${id}`);
 }
 
+let numOfObj = 0;
 const objArray = [];
+const iterateButton = document.getElementById('iterate');
+const firstObj = document.getElementById('first');
+const lastObj = document.getElementById('last');
+
+// iterateButton.addEventListener('click', iterateObj);
