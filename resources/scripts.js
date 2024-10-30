cdb47fdbbccb51a8d057a60e2573cb6784cf9722
smashblu <smashblu@gmail.com>
2024-10-30 14:57:28 -0700

    Log new messaging issues

--- resources/scripts.js
+++ resources/scripts.js
@@ -374,12 +374,10 @@ async function moneyChange(newBet) {
 
 function buildSummary(msg, rolled) {
     // Issues
-    // Win place bet: Says 'no action'
-    // 7 with secondary bets active: No message displays
-    // Craps pre point: Says 'place bets lose'
-    // 7/11 pre point: Says 'point wins'
-    // Come bet 7/11: Says 'no action'
-    // Push secondary bets: Iterates a (blank) message for bets even though amount < 1
+    // Logging push/loss secondary bets creates message for each even if no bet was on the number (pushSecondaryBets)
+    // However logging is correct but adds extra "have pushed _" messages
+    // 7/11 says "point won"
+    // Does not tell player come bets win/lose
     switch (msg) {
         case SHOWPOINT:
             msg += ` ${playerPoint}`;
